abc7ca32d4ddefa7820aea321dd03fb3157defa6
Cooper Saye <csaye2022@chadwickschool.org>
2020-05-07 20:49:53 -0700

    Improve visuals.

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+
+    public Animator textAnimator;
+
+    private float frames = 60;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (frames == -1) textAnimator.SetBool("TextExpand", false);
+        if (frames == 0) {
+
+            frames = -1;
+
+            // Trigger start text
+            textAnimator.SetBool("TextExpand", true);
+        }
+        if (frames > 0) frames--;
+    }
+}
